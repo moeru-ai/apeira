8de33a47dfa13ff7b8be2e34e8d50681eb1f6435
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-05-18 13:37:26 +0800

    chore(deps): add copilotkit

--- examples/copilotkit/src/app.tsx
+++ examples/copilotkit/src/app.tsx
@@ -1,3 +1,11 @@
+import { CopilotChat } from '@copilotkit/react-core/v2'
+
+import '@copilotkit/react-ui/v2/styles.css'
+
 export const App = () => (
-  <p>Hello</p>
+  <CopilotChat
+    labels={{
+      welcomeMessageText: 'Hi! How can I assist you today?',
+    }}
+  />
 )
